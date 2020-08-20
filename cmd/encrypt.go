47b39eee54d312f70c8c8e1725f0ac92386a0e27
lbejiuk <sergeyzarva@gmail.com>
2020-08-20 21:36:01 +0200

    add encryption sub command

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -3,45 +3,97 @@ package cmd
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"fmt"
 	"github.com/spf13/cobra"
 	"goCkup/utils"
 	"io"
 	"log"
 	"os"
-	"strings"
 )
 
-// goCkup encrypt _fileName_
+// goCkup encrypt _fileName_ _fileNameOutput_ <- random generated key. Should be logged out at the end.
+// goCkup encrypt --key='32bitkey' _fileNameInput_ _fileNameOutput_
+// goCkup encrypt --keyFile='32bitkey' _fileName_ _fileNameOutput_
+// todo suggest for bash
 var CmdEncrypt = &cobra.Command{
-	Use:   "encrypt _todo_",
-	Short: "_todo_",
-	Long:  `_todo_`,
+	Use:   "encrypt _filename_",
+	Short: "Take _filename_ ane encrypt it",
+	Long:  `Take _filename_ ane encrypt it`,
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Echo: " + strings.Join(args, " "))
+		encrypt(args, encryptionKeyFlag, encryptionKeyFileFlag)
 	},
 }
+var encryptionKeyFlag string
+var encryptionKeyFileFlag string
 
-func main_() {
+func init() {
+	CmdEncrypt.Flags().StringVar(&encryptionKeyFlag, "key", "", "key for encryption")
+	CmdEncrypt.Flags().StringVar(&encryptionKeyFileFlag, "keyFile", "", "key for encryption.!!!! Should be 32 Byte!!!")
+}
 
-	key := []byte("AES256Key-32Characters1234567890")
+func encrypt(args []string, encryptionKeyFlag string, encryptionKeyFileFlag string) {
+	encryptionKey := make([]byte, 32)
+	if len(encryptionKeyFlag) == 0 {
+		if len(encryptionKeyFileFlag) != 0 {
+			// try to open file
+			encryptionKeyFile, err := os.Open(encryptionKeyFileFlag)
+			if err != nil {
+				log.Fatalf("Can not open key file. %v", err)
+			}
+			defer encryptionKeyFile.Close()
+			tmp := make([]byte, 33)
+			count, err := encryptionKeyFile.Read(tmp)
+			if err != nil {
+				log.Fatalf("Can not read key file. %v", err)
+			}
+			if count < 32 {
+				log.Fatalf("Key should be 32 bytes")
+			} else if count > 32 {
+				log.Print("Key should be 32 bytes. Only the first 32 bytes will be used.")
+			}
+			encryptionKey = tmp[:32]
+		}
+	} else {
+		if len(encryptionKeyFileFlag) != 0 {
+			log.Print("The keyFile does not matter. The key has priority over the keyFile.")
+		}
+		if len(encryptionKeyFlag) < 32 {
+			log.Fatalf("Key should be 32 bytes")
+		} else if len(encryptionKeyFlag) > 32 {
+			log.Print("Key should be 32 bytes. Only the first 32 bytes will be used.")
+		}
+		encryptionKey = []byte(encryptionKeyFlag[:32])
+	}
+	if len(encryptionKey) == 0 {
+		encryptionKey = utils.MakeRandom(32)
+		log.Print("The key wasn't handed over. It will be generated automatically. Key:", encryptionKey)
+	}
 
-	fileToEncode, _ := os.Open("2")
+	fileToEncode, _ := os.Open(args[0])
 	defer fileToEncode.Close()
-	encodedFile, _ := os.OpenFile("./encoded", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	encodedFileName := "./encoded"
+	if len(args) > 1 && len(args[1]) != 0 {
+		encodedFileName = args[1]
+	}
+	encodedFile, err := os.OpenFile(encodedFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Can not open encrypted file. %v", err)
+	}
 	defer encodedFile.Close()
 
-	block, err := aes.NewCipher(key)
+	block, err := aes.NewCipher(encryptionKey)
 	if err != nil {
 		log.Fatalf("Unable to create Chiper. %v", err)
 	}
 	iv := utils.MakeRandom(aes.BlockSize)
 	stream := cipher.NewOFB(block, iv[:])
-	encodedFile.Write(iv)
+	_, err = encodedFile.Write(iv)
+	if err != nil {
+		log.Fatalf("Can not write encrypted file. %v", err)
+	}
 	writer := &cipher.StreamWriter{S: stream, W: encodedFile}
 	if _, err := io.Copy(writer, fileToEncode); err != nil {
-		panic(err)
+		log.Fatalf("Error during encription. %v", err)
 	}
 }
 
